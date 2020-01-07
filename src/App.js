8264090c61d1bc7a64b8354bf378ba36b8673854
AKaarna <AKaarna@AccountGt>
2020-01-07 11:32:43 +0200

    routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 //import ReactDOM from 'react-dom';
 import {
-   BrowserRouter as Router,
-   Switch,
-   Route,
-   Link
- } from "react-router-dom";
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
 import Home from './Home.js';
 import About from './About.js';
-import Contact from './Contact.js';
+//import Contact from './Contact.js';
+import Topics from './Topics.js';
 
 class App extends Component {
    constructor(props) {
@@ -19,32 +22,34 @@ class App extends Component {
    };
    render() {
       return (
-         <Router>
-            <div>
-               <nav>
-                  <ul>
-                     <li>
-                        <Link to="/">Home</Link>
-                     </li>
-                     <li>
-                        <Link to="/about">About</Link>
-                     </li>
-                     <li>
-                        <Link to="/contact">Contact</Link>
-                     </li>
-                  </ul>
-               </nav>
-    
-               {/* A <Switch> looks through its children <Route>s and
-                  renders the first one that matches the current URL. */}
-               <Switch>
-                  <Route exact path="/"><Home /></Route> {/*component={Home} />*/}
-                  <Route path="/about" component={About} />
-                  <Route path="/contact" component={Contact} />
-               </Switch>
-            </div>
-         </Router>
-      );
+        <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/topics">Topics</Link>
+            </li>
+          </ul>
+  
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/topics">
+              <Topics />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );
    }
 }
 export default App;
